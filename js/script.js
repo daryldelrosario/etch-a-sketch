7c141e29dfcf069175aa294e2826a0c1a320684a
Daryl del Rosario <daryldelrosario.is@gmail.com>
2022-11-13 18:18:05 -0500

    Change div color on hover

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
-const board = document.querySelector(".board");
+let board = document.querySelector(".board");
 board.style.gridTemplateColumns = "repeat(16, 1fr)";
 board.style.gridTemplateRows = "repeat(16, 1fr)";
 
 for(let i = 0; i < 256; i++) {
     let square = document.createElement("div");
+
+    square.addEventListener("mouseover", function () {
+        square.style.background = "black";
+    });
+
     square.style.border = "1px solid black";
-    square.style.backgroundColor = "white";
+    square.style.background = "white";
     board.insertAdjacentElement("beforeend", square);
 }
